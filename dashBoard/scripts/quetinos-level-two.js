2a701d78b07164e15b2da1ecf31fc6e66356f9c1
bilal-hamdan <bilalhamdan99@gmail.com>
2024-06-19 15:27:01 +0300

    add and delete level questions

--- dashBoard/scripts/quetinos-level-two.js
+++ dashBoard/scripts/quetinos-level-two.js
@@ -1,17 +1,25 @@
+const table = document.querySelector("table tbody");
 let currentEditButton;
-
-function addQuestion() {
-  const questionText = document.getElementById('questionContext').value;
-  const questionAnswer = document.getElementById('questionAnswer').value;
-  if (questionText && questionAnswer) {
-    const table = document.querySelector('table tbody');
-    const row = document.createElement('tr');
-
-    row.innerHTML = `
-                    <td>1</td>
-                    <td>${questionText}</td>
-                    <td>${questionAnswer}</td>
-                    <td>0</td>
+//getting local storage level-one data
+let allQuestions = JSON.parse(localStorage.getItem("level-two"));
+//loading all questions
+onload = loadAllQuestios();
+function loadAllQuestios() {
+  //checking if there are any questions in local storage
+  let keys = Object.keys(allQuestions);
+  if (keys.length >= 3) {
+    for (const q in allQuestions) {
+      //skipping the data of "levelID" and "started" keys
+      if (q != "levleID" && q != "started") {
+        let qText = allQuestions[q]["questionContext"];
+        let qAnswer = allQuestions[q]["correctAnswers"];
+        //creating a new table row and adding new questions to the table
+        let row = document.createElement("tr");
+        row.innerHTML = `
+                    <td>${q}</td>
+                    <td>${qText}</td>
+                    <td>${qAnswer}</td>
+                    
                   
                      <td>
                 <button class="edit-button" onclick="openEditModal(this)">
@@ -24,31 +32,48 @@ function addQuestion() {
                 </button>
               </td>
                 `;
-    table.appendChild(row);
+        table.appendChild(row);
+
+        console.log(allQuestions[q]["questionContext"]);
+      }
+    }
   }
 }
 
+function addQuestion() {
+  const questionText = document.getElementById("questionContext").value;
+  const questionAnswer = document.getElementById("questionAnswer").value;
+  if (questionText && questionAnswer) {
+    createQuestions(questionText, questionAnswer, 2);
+    location.reload();
+  }
+}
 function deleteQuestion(button) {
   const row = button.parentNode.parentNode;
+  //getting the question id
+  QID = row.cells[0];
+  `1`;
+  console.log(QID.innerText);
+  deleteLevelQuestion(2, QID.innerText);
   row.parentNode.removeChild(row);
 }
 
 function openEditModal(button) {
   currentEditButton = button;
-  document.getElementById('editModal').style.display = 'block';
+  document.getElementById("editModal").style.display = "block";
   const questionText = button.parentNode.parentNode.children[1].textContent;
   const questionAnswer = button.parentNode.parentNode.children[2].textContent;
-  document.getElementById('editText').value = questionText;
-  document.getElementById('editAnswer').value = questionAnswer;
+  document.getElementById("editText").value = questionText;
+  document.getElementById("editAnswer").value = questionAnswer;
 }
 
 function closeEditModal() {
-  document.getElementById('editModal').style.display = 'none';
+  document.getElementById("editModal").style.display = "none";
 }
 
 function saveEdit() {
-  const newText = document.getElementById('editText').value;
-  const newQuestionAnswer = document.getElementById('editAnswer').value;
+  const newText = document.getElementById("editText").value;
+  const newQuestionAnswer = document.getElementById("editAnswer").value;
   if (currentEditButton && newText && newQuestionAnswer) {
     currentEditButton.parentNode.parentNode.children[1].textContent = newText;
     currentEditButton.parentNode.parentNode.children[2].textContent =
@@ -58,7 +83,7 @@ function saveEdit() {
 }
 
 window.onclick = function (event) {
-  if (event.target == document.getElementById('editModal')) {
+  if (event.target == document.getElementById("editModal")) {
     closeEditModal();
   }
 };
--- scripts/set-local-storage-data.js
+++ scripts/set-local-storage-data.js
@@ -1,67 +1,27 @@
 localStorage.setItem(
-  'participants',
+  "participants",
   `
   {}
 `
 );
 //testing
-localStorage.setItem('currentParticipantID', '1');
+localStorage.setItem("currentParticipantID", "1");
 
 localStorage.setItem(
-  'level-two',
+  "level-two",
   ` {
   "levleID": "2",
-  "started": "0",
-  "1": {
-    "questionContext": "example of the question text",
-    "correctAnswers": "300",
-    "totalCorrectAnswers": 0
-  },
-  "2": {
-    "questionContext": "example of the question text",
-    "correctAnswers": "900",
-    "totalCorrectAnswers": 0
-  },
-  "3": {
-    "questionContext": "example of the question text",
-    "correctAnswers": "100",
-    "totalCorrectAnswers": 0
-  },
-      "4": {
-    "questionContext": "example of the question text",
-    "correctAnswers": "100",
-    "totalCorrectAnswers": 0
-  },
-    "5": {
-    "questionContext": "example of the question text",
-    "correctAnswers": "100",
-    "totalCorrectAnswers": 0
-  }
+  "started": "0"
 }
 
 
 `
 );
 localStorage.setItem(
-  'level-one',
+  "level-one",
   `{
   "levleID": "1",
-  "started": "0",
-  "1": {
-    "questionContext": "example of the question text",
-    "correctAnswers": "300",
-    "totalCorrectAnswers": 0
-  },
-  "2": {
-    "questionContext": "example of the question text",
-    "correctAnswers": "900",
-    "totalCorrectAnswers": 0
-  },
-  "3": {
-    "questionContext": "example of the question text",
-    "correctAnswers": "100",
-    "totalCorrectAnswers": 0
-  }
+  "started": "0" 
 }
 
 
@@ -69,7 +29,7 @@ localStorage.setItem(
 );
 
 localStorage.setItem(
-  'admin',
+  "admin",
   `
   {
   "admins": {
