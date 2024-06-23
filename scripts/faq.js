9360f61c88a6acbad9e2643534f99c31ebba32b4
abdulazeez761 <abd828981@gmail.com>
2024-06-23 20:01:24 +0300

    ...

---
+++ scripts/faq.js
@@ -0,0 +1,7 @@
+// -----------------------FQAs start------------
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach((faq) => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('active');
+  });
+});
--- utils/navigateTo.js
+++ utils/navigateTo.js
@@ -1,11 +1,3 @@
 function navigateTo(page) {
   window.location.href = page;
 }
-
-// -----------------------FQAs start------------
-const faqs=document.querySelectorAll(".faq");
-faqs.forEach(faq=> {
- faq.addEventListener("click",()=>{
-     faq.classList.toggle("active");
- });
-});
