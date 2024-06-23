d6442f09ef806d7bafe008e82916a0ac11c6158a
WesalAlshamali <wesalmazen4@gmail.com>
2024-06-23 19:47:12 +0300

    cc

--- utils/navigateTo.js
+++ utils/navigateTo.js
@@ -1,3 +1,11 @@
 function navigateTo(page) {
   window.location.href = page;
 }
+
+// -----------------------FQAs start------------
+const faqs=document.querySelectorAll(".faq");
+faqs.forEach(faq=> {
+ faq.addEventListener("click",()=>{
+     faq.classList.toggle("active");
+ });
+});
